a652636115f2c28e43458b278e8e8868acc118f8
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-10 15:02:17 +0600

    add at a time multiple data

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -24,7 +24,14 @@ router.post("/", async (req, res) => {
 });
 
 //post all the todo
-router.post("/all", async (req, res) => {});
+router.post("/all", async (req, res) => {
+  try {
+    await toDo.insertMany(req.body);
+    res.status(200).json({ message: "Todos were inserted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 //put a todo
 router.put("/:id", async (req, res) => {});
