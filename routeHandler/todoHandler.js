27d7e18097cfd18b13473e3bd01e53ffce81cd8e
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-10 10:01:29 +0600

    create mongoose model

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const todoSchema = require("../schemas/todoSchema");
+
+//create mongoose model
+const toDo = mongoose.model("toDo", todoSchema);
 
 //get all the todo
 router.get("/", async (req, res) => {});
--- schemas/todoSchema.js
+++ schemas/todoSchema.js
@@ -1,1 +1,19 @@
 const mongoose = require("mongoose");
+
+const todoSchema = mongoose.Schema({
+  title: {
+    type: string,
+    required: true,
+  },
+  description: string,
+  status: {
+    type: string,
+    enum: ["active", "inActive"],
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = todoSchema;
