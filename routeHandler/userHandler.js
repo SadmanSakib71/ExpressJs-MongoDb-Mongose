ecfa761e74f0b4345039f02bc7c39577c0ef80d6
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-14 05:48:00 +0600

    succefully signed up

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 const userSchema = require("../schemas/userSchema");
 
@@ -9,6 +10,19 @@ const user = mongoose.model("user", userSchema);
 //routes without id
 
 //signUp
-router.post("/", async (req, res) => {});
+router.post("/signUp", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new user({
+      name: req.body.name,
+      userName: req.body.userName,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
