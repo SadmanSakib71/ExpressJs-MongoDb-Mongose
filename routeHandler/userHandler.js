df8f54643881f61c28994b53c3daf1abc7437f58
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-14 06:10:16 +0600

    installed jwt and validate for login

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
 const router = express.Router();
 const userSchema = require("../schemas/userSchema");
 
@@ -22,6 +23,27 @@ router.post("/signUp", async (req, res) => {
     res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
     res.status(500).json({ error: err.message });
+  }
+});
+
+//logIn
+router.post("/login", async (req, res) => {
+  try {
+    const checkUser = await user.findOne({ userName: req.body.userName });
+    if (checkUser) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        checkUser.password,
+      );
+      if (isValidPassword) {
+      } else {
+        res.status(401).json({ error: "Authentication failed" });
+      }
+    } else {
+      res.status(401).json({ error: "Authentication failed" });
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
